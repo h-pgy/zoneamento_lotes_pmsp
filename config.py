b0e381a7feba6600b11629bc8b4642f3abafcd4e
Henrique Pougy <hpougy@SF.PREFEITURA.SP.GOV.BR>
2026-04-24 12:10:54 -0300

    classe para fazer download dos dados do iptu

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent
 DATA_DIR = BASE_DIR / "data"
 OUTPUT_DIR = BASE_DIR / "output"
+FILE_IPTU_GEOSAMPA = "12_Cadastro\\\\IPTU_INTER\\\\XLS_CSV\\\\IPTU_2026"
